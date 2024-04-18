938210c091cf5b343fbaea69c453d403035296c3
Jyoti Raut <jraut3434@gmail.com>
2024-04-18 13:23:47 +0545

    setting the environment for backend

---
+++ backend/src/app.js
@@ -0,0 +1,39 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import dotenv from 'dotenv';
+dotenv.config({ path: "./.env" });
+
+import database from './db/database.js';
+import userRouter from './routes/userRoutes.js';
+
+
+
+const app = express();
+
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(cookieParser())
+
+
+//using the cors
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+database();
+
+//using the routes 
+app.use('/api/v1/users',userRouter);
+
+const PORT = 3000;
+
+app.listen(PORT,()=>{
+ console.log(`server is running in the port ${PORT}`);
+})
+
+export {app};
