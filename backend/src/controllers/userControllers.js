f1f7446f9950c938be235f804b374bd00a7b44b8
Jyoti Raut <jraut3434@gmail.com>
2024-04-18 14:27:13 +0545

    Register User and encryption

--- backend/src/controllers/userControllers.js
+++ backend/src/controllers/userControllers.js
@@ -1,16 +1,29 @@
 import User from '../models/userModels.js'
+import bcrypt from "bcrypt"
 
 const registerUser= async(req,res)=>{
     try{
         const {username,email,password,confirmPassword} = req.body;
         console.log(req.body);
 
+        const existingUser = await User.findOne({email:email});
+        if(existingUser){
+            res.status(400).json({error:"Email already exist"})
+        }
+
+        const hashedPassword = await bcrypt.hash(password,10);
+        if(password!= confirmPassword){
+            res.status(401).json({error:"password and Confirm password doesnot match"});
+        }
+
         const user = new User({
             username,
             email,
-            password,
+            password : hashedPassword,
             confirmPassword
         })
+
+        
         await user.save();
         res.status(201).json({message:"User registered successfully"});
     }
