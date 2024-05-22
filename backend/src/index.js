6168cabe11835ec060efc6a5a694a2e1b668cc7b
shreeyatamang <shreeya.211333@ncit.edu.np>
2024-05-22 18:44:29 +0545

    databse changed

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,15 +4,12 @@ import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
 import path from 'path';
-dotenv.config({ path: "./.env" });
-
-
-import database from './db/database.js';
+import { connectToMongoDB } from './db/database.js' // Importing named export
 import userRouter from './routes/userRoutes.js';
 import eventRouter from './routes/eventRoutes.js';
 import volunteerRouter from './routes/volunteerRoutes.js';
 
-
+dotenv.config({ path: "./.env" });
 
 const app = express();
 
@@ -20,28 +17,35 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("./public"));
 
-app.use(cookieParser())
+app.use(cookieParser());
 
-
-//using the cors
+// Using CORS
 app.use(
   cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true,
   })
 );
-database();
 
-//using the routes 
-app.use('/api/v1/users',userRouter);
-app.use('/api/v1/events',eventRouter);
-app.use('/api/v1/volunteers',volunteerRouter);
+// Connecting to MongoDB
+connectToMongoDB()
+  .then(() => {
+    // MongoDB connected successfully
+    console.log("Connected to MongoDB Atlas");
+    
+    // Using routes
+    app.use('/api/v1/users', userRouter);
+    app.use('/api/v1/events', eventRouter);
+    app.use('/api/v1/volunteers', volunteerRouter);
 
+    // Starting the server
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB Atlas:", err);
+  });
 
-const PORT = 3000;
-
-app.listen(PORT,()=>{
- console.log(`server is running in the port ${PORT}`);
-})
-
-export {app};
+export { app };
